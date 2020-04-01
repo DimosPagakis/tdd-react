25d481ff0869d889fe804f287cae13a6a9cdae21
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-01 19:54:33 +0300

    Fixed a in a warning

--- src/App.js
+++ src/App.js
@@ -15,32 +15,23 @@ export default class App extends React.Component {
     return (<div id="App">
       <Router>
         <Navbar expand="lg" variant="dark" bg="dark">
-          <Container fluid="true">
-            <Row>
-              <Col md={2} sm={2} xs={2}>
-                <Navbar.Brand>TDD</Navbar.Brand>
-              </Col>
 
-              <Col md={6} sm={4} xs={2}></Col>
+          <Row>
+            <Col md={2} sm={2} xs={2}>
+              <Navbar.Brand>TDD</Navbar.Brand>
+            </Col>
 
-              <Col md={4} sm={6} xs={8}>
-                <Nav className="mr-auto">
-                  <Nav.Link>
-                    <Link to="/">Home</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/haris">Korponents</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/bench">BenchComponents</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/sheep">SheepComponents</Link>
-                  </Nav.Link>
-                </Nav>
-              </Col>
-            </Row>
-          </Container>
+            <Col md={6} sm={4} xs={2}></Col>
+
+            <Col md={4} sm={6} xs={8}>
+              <Nav className="mr-auto">
+                <Link to="/">Home</Link>
+                <Link to="/haris">Korponents</Link>
+                <Link to="/bench">BenchComponents</Link>
+                <Link to="/sheep">SheepComponents</Link>
+              </Nav>
+            </Col>
+          </Row>
         </Navbar>
 
         <Switch>
