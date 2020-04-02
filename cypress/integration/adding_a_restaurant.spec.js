b062ef277c52726c0a468477466bbf48cc1aed72
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-02 20:04:01 +0300

    Break e2e test into functions

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -1,12 +1,23 @@
 describe('adding a restaurant', () => {
+  const restaurantName = 'Sushi Place';
   it('displays the reastaurant in thet list', () => {
-    const restaurantName = 'Sushi Place';
 
     cy.visit('http://localhost:1234');
 
+    modalNotShown();
+
+    canCloseModal();
+
+    listsValidRestaurantName();
+  });
+
+  function modalNotShown() {
     // confirm newRestaurantName field not shown
     cy.get('[data-test="newRestaurantName"]').should('not.be.visible');
 
+  }
+
+  function canCloseModal() {
     // open and close modal without adding a new restaurant
     cy.get('[data-test="addRestaurantButton"]')
       .click();
@@ -16,6 +27,10 @@ describe('adding a restaurant', () => {
 
     cy.get('[data-test="newRestaurantName"]').should('not.be.visible');
 
+    displaysErrorOnBlankInput();
+  }
+
+  function displaysErrorOnBlankInput() {
     // modal displays validation errors
     cy.get('[data-test="addRestaurantButton"]')
       .click();
@@ -28,7 +43,9 @@ describe('adding a restaurant', () => {
 
     cy.get('[data-test="newRestaurantNameError"]')
       .should('have.css', 'color', 'rgb(255, 0, 0)');
+  }
 
+  function listsValidRestaurantName() {
     /**
      * Type restaurant name force true so it does not throw an error because label is shown
      */
@@ -42,5 +59,5 @@ describe('adding a restaurant', () => {
     cy.get('[data-test="newRestaurantName"]').should('not.be.visible');
 
     cy.contains(restaurantName);
-  });
+  }
 });
